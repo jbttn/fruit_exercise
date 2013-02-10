4eff7cd95552960f758386b0990385f768189059
Joshua Button <deathnoodle@gmail.com>
2013-02-10 00:17:53 -0800

    Updated dataset migration.

--- db/migrate/001_create_dataset.rb
+++ db/migrate/001_create_dataset.rb
@@ -1,6 +1,6 @@
 Sequel.migration do
   change do
-    create_table(:dataset) do
+    create_table(:datasets) do
       primary_key :id
       String   :url, null: false
       String   :referrer, null: true
